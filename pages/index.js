c3caf2e18b154102036acfb2aa5d790fc70183ef
prnvbirajdar <prnvbirajdar@gmail.com>
2021-01-13 01:34:25 -0500

    login done

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
+import Nav from "../components/nav";
 
 export default function IndexPage() {
   return (
     <div>
-      <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-        Next.js + Tailwind CSS 2.0
-      </h1>
+      <div className="my-36">
+        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+          Welcome to Lift
+        </h1>
+        <h3 className="text-center my-4">Train harder. Track smarter.</h3>
+
+        <div className="flex justify-center mt-20">
+          <button className="text-blue-500 mr-6">Register</button>
+          <button className="px-5 py-2 font-bold text-white bg-blue-500 rounded">
+            Log In
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
